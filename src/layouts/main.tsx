11344aed3098d0a4b7806df9cc3dc53e6be7cbe4
Alisher Uralov <aliwerdev@gmail.com>
2024-08-07 17:36:39 +0500

    di

--- src/layouts/main.tsx
+++ src/layouts/main.tsx
@@ -1,10 +1,14 @@
 import { Outlet } from "react-router-dom";
+import SimpleBar from "simplebar-react";
+import "simplebar-react/dist/simplebar.min.css";
 
 const MainLayout = () => {
   return (
-    <div className="max-w-[900px] mx-auto px-2 sm:px-4">
-      <Outlet />
-    </div>
+    <SimpleBar style={{ maxHeight: "99vh" }}>
+      <div className="max-w-[900px] mx-auto px-2 sm:px-4">
+        <Outlet />
+      </div>
+    </SimpleBar>
   );
 };
 
