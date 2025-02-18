bd06b9689747b850f6612c7e9214c4884772cb5f
Alisher Uralov <aliwerdev@gmail.com>
2025-02-18 14:52:41 +0500

    Add iframe integration and dynamic height adjustment: implement ResizeObserver for body height tracking and post messages to parent window

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { App as AntdApp } from "antd";
 
 import Router from "./routes";
 import AntProvider from "theme/ant-provider";
+import { useEffect, useState } from "react";
 
 // ----------------------------------------------------------------------
 
@@ -15,6 +16,33 @@ const queryClient = new QueryClient({
   },
 });
 export default function App() {
+  const [height, setHeight] = useState(0);
+
+  // Listen for changes to the document body height.
+  useEffect(() => {
+    const observer = new ResizeObserver((entries) => {
+      for (const entry of entries) {
+        setHeight(entry.contentRect.height);
+      }
+    });
+    const body = document.querySelector("body");
+    if (body) observer.observe(body);
+
+    return () => {
+      observer.disconnect();
+    };
+  }, []);
+
+  // Send a post message to the parent window with the current document body height.
+  useEffect(() => {
+    const isIframe = window.self !== window.top;
+    if (isIframe) {
+      document.documentElement.style.overflow = "hidden";
+      window.parent.postMessage(height, "*");
+    } else {
+      document.documentElement.style.overflow = "unset";
+    }
+  }, [height]);
   return (
     <QueryClientProvider client={queryClient}>
       <AntProvider>
