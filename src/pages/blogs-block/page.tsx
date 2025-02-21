1cb6bb0a935f75ef5685fbb1681d704f18ef3980
Alisher Uralov <aliwerdev@gmail.com>
2025-02-21 11:31:51 +0500

    Update blog data retrieval to use POST request with sorting by creation time

--- src/pages/blogs-block/page.tsx
+++ src/pages/blogs-block/page.tsx
@@ -13,7 +13,18 @@ import { get } from "lodash";
 import axiosInstance from "services/axios";
 
 const BlogsBlockPage = () => {
-  const { data: blogsData } = useQuery({ queryKey: ["blogs"], queryFn: async () => await axiosInstance.get("https://notion-api.pdp.uz/?database_id=19df4e0aaef380a0938ec4307b2b6b24") });
+  const { data: blogsData } = useQuery({
+    queryKey: ["blogs"],
+    queryFn: async () =>
+      await axiosInstance.post("https://notion-api.pdp.uz/?database_id=19df4e0aaef380a0938ec4307b2b6b24", {
+        sorts: [
+          {
+            property: "Created time",
+            direction: "descending",
+          },
+        ],
+      }),
+  });
 
   const blogs = useMemo(() => {
     let data: BlogPost[] = parseNotionResponse(get(blogsData, "data", []));
